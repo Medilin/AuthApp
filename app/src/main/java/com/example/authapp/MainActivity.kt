8ab40329bb6ed2499561a9fd69d8eeee72f8d872
Manhunterrr <95933815+Manhunterrr@users.noreply.github.com>
2023-03-20 21:24:47 +0500

    Activity buttons are made functional with their respective event handler

--- app/src/main/java/com/example/authapp/MainActivity.kt
+++ app/src/main/java/com/example/authapp/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.authapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val startButton = findViewById<Button>(R.id.button)
+        startButton.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
     }
+
+
+
+
+
 
 
 
